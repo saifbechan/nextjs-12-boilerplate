9f808f558249532c7b9b3d4a7719286407a4f5ab
Saif Bechan <saifbechan@gmail.com>
2022-07-12 13:35:27 +0200

    chore: added contentful

---
+++ getContentfulEnvironment.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const contentfulManagement = require('contentful-management');
+
+module.exports = function () {
+  const contentfulClient = contentfulManagement.createClient({
+    accessToken: process.env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN,
+  });
+
+  return contentfulClient
+    .getSpace(process.env.CONTENTFUL_SPACE_ID)
+    .then((space) => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT));
+};
